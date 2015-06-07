670fb02a55f972fdde9736de43207625b63a5a2c
Olivier <oliv.nguyen@hotmail.fr>
2015-06-07 22:17:55 +0200

    Begin ranking algporithm

--- src/Pouce/SiteBundle/Controller/RankingController.php
+++ src/Pouce/SiteBundle/Controller/RankingController.php
@@ -11,9 +11,30 @@ class RankingController extends Controller
     //Show the "normal" ranking
     public function showRanking()
     {
-        $team=getAllTeamsInEdition(getLastIdEdition());
+        $teams=getAllTeamsInEdition(/*  getIdITheAdress  */);
         return $this->render('PouceSiteBundle:Site:ranking.html.twig', array(
-          'team' => $team,
+          'teams' => $teams,
         ));
+    }
+
+    //Recalcule toutes les rangs et stock les nouvelles valeurs dans la base de données 
+    private function rankingCalculus($teams){
+        $rang=0;
+        $rangAdd=0;
+        $ancienneDistance=0;
+            while($team)
+            {
+                if($team->getDistance()!=$ancienneDistance)
+                {
+                    $rang+=1;
+                    $rang+=$rangAdd;
+                    $rangAdd=0;
+                }
+                else
+                {
+                    $rangAdd+=1;
+                }
+                $ancienneDistance=$team->getDistance();
+            }
     }
 }
