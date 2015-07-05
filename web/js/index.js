4935113ed808ce665b7f6ec08912cef785796914
Olivier <oliv.nguyen@hotmail.fr>
2015-07-05 22:34:49 +0200

    New design for index

--- web/js/index.js
+++ web/js/index.js
@@ -1,25 +1,43 @@
+$( document ).ready(function(){
+	$('#navbar').css('position', 'absolute');
+	$('#content').removeClass("container");
+	$('.section').css('padding-top','2.5em');
+	$('.header').css('margin-top','2em');
+	$('#fullpage').fullpage(
+	{
+		onLeave: function(index, nextIndex, direction){
+		    var leavingSlide = $(this);
+
+            if(index == 1 && direction == 'down'){
+            	$("#navcolor").attr('class', 'white');
+            }
+            if(index == 2 && direction == 'up'){
+            	$("#navcolor").attr('class', 'transparent');
+            }
+        },
+        //Scrolling
+
+        //Accessibility
+        keyboardScrolling: true,
+        scrollBar: true,
+
+        //Design
+        controlArrows: true,
+        navigation: true,
+        navigationPosition: 'right',
+
+        // fixedElements: '#header, .footer',
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        responsiveWidth: 800,
+    });
+});
+
 $('#index-banner').on('scrollSpy:enter', function() {
     $("#navcolor").attr('class', 'transparent');
 });
 
-$('#index-banner').on('scrollSpy:exit', function() {
-    $("#navcolor").attr('class', 'white');
-});
-
 $('#index-banner').scrollSpy();
-
-// $('#top').on('scrollSpy:enter', function() {
-// 	$('body').scrollTo('#content3blocks');
-// });
-
-
-// $('#top').on('scrollSpy:exit', function() {
-//     $('body').scrollTo('#content3blocks');
-// });
-
-// $('#top').scrollSpy();
-
-$( document ).ready(function(){
-	$('#navbar').css('position', 'absolute');
-	$('#content').removeClass("container");
-});
