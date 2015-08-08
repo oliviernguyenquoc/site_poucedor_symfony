593cd5a9384c205078308b56ef52e3864dc65647
Olivier <oliv.nguyen@hotmail.fr>
2015-08-08 20:04:41 +0200

    Add previous ranking and pdf

--- src/Pouce/SiteBundle/Controller/SiteController.php
+++ src/Pouce/SiteBundle/Controller/SiteController.php
@@ -6,58 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SiteController extends Controller
 {
-	public function indexAction()
-    {
-        return $this->render('PouceSiteBundle:Site:index.html.twig');
-    }
-    public function reglesAction()
-    {
-        return $this->render('PouceSiteBundle:Site:regles.html.twig');
-    }
-        public function archivesAction()
-    {
-        return $this->render('PouceSiteBundle:Site:archives.html.twig');
-    }
-    public function assoAction()
-    {
-        return $this->render('PouceSiteBundle:Site:asso.html.twig');
-    }
-    public function conseilsAction()
-    {
-        return $this->render('PouceSiteBundle:Site:conseils.html.twig');
-    }
-    public function editionAction()
-    {
-        return $this->render('PouceSiteBundle:Site:edition.html.twig');
-    }
-    public function editionPrecedenteAction()
-    {
-        return $this->render('PouceSiteBundle:Site:editions_precedentes.html.twig');
-    }
-    public function liensAction()
-    {
-        return $this->render('PouceSiteBundle:Site:liens.html.twig');
-    }
-    public function organisateursAction()
-    {
-        return $this->render('PouceSiteBundle:Site:organisateurs.html.twig');
-    }
-    public function videosAction()
-    {
-        return $this->render('PouceSiteBundle:Site:videos.html.twig');
-    }
-    public function recordsAction()
-    {
-        return $this->render('PouceSiteBundle:Site:records.html.twig');
-    }
-    public function licenceAction()
-    {
-        return $this->render('PouceSiteBundle:Site:licence.html.twig');
-    }
-    public function inscriptionEcoleAction()
-    {
-        return $this->render('PouceSiteBundle:Site:inscriptionEcole.html.twig');
-    }
     public function galeryAction($idEdition)
     {
         $dir = $this->get('kernel')->getRootDir(). '/../web/photos/' . $idEdition;
@@ -81,10 +29,12 @@ class SiteController extends Controller
             }
         }
             
-
-
         return $this->render('PouceSiteBundle:Site:galery.html.twig', array(
           'imageArray'    => $imageArray
         ));
+    }
+    public function classementAction($annee)
+    {
+        return $this->render('PouceSiteBundle:Site:classement_'.$annee.'.html.twig');
     }
 }
