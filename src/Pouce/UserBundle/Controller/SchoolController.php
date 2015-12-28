716ab29ef4298bb34a17065cd435a6f86a37ed27
Olivier <oliv.nguyen@hotmail.fr>
2015-12-28 01:56:39 +0100

    Add edit school

--- src/Pouce/UserBundle/Controller/SchoolController.php
+++ src/Pouce/UserBundle/Controller/SchoolController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Pouce\UserBundle\Entity\School;
 
 use Pouce\UserBundle\Form\Type\SchoolType;
+use Pouce\UserBundle\Form\Type\SchoolEditType;
 
 class SchoolController extends Controller
 {
@@ -33,8 +34,32 @@ class SchoolController extends Controller
 
         // On passe la méthode createView() du formulaire à la vue
         // afin qu'elle puisse afficher le formulaire toute seule
-        return $this->render('PouceSuperAdminBundle:Admin:addSchool.html.twig', array(
+        return $this->render('PouceSuperAdminBundle:Admin:editSchool.html.twig', array(
           'form' => $form->createView()
+        ));
+    }
+
+    public function editSchoolAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $school = $em ->getRepository('PouceUserBundle:School')->find($id);
+    
+        $form = $this->get('form.factory')->create(new SchoolEditType(), $school);
+
+        if($request->getMethod() == 'POST') {
+            $form->bind($request);
+
+            if($form->isValid()){
+                //On enregistre la team
+                $em->persist($school);
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('pouce_site_config'));
+            }
+        }
+        return $this->render('PouceSuperAdminBundle:Admin:editSchool.html.twig', array(
+          'form'        => $form->createView(),
+          'school'   => $school
         ));
     }
 
--- src/Pouce/UserBundle/Form/Type/SchoolEditType.php
+++ src/Pouce/UserBundle/Form/Type/SchoolEditType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class SchoolEditType extends EditionType
+class SchoolEditType extends SchoolType
 {
 
     /**
