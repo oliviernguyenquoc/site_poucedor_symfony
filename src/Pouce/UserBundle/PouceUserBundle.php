2a1841ee9e39d4304ad148aae84d6ccb81bce718
Olivier <oliv.nguyen@hotmail.fr>
2016-04-23 09:24:58 +0200

    Wsse first commit

--- src/Pouce/UserBundle/PouceUserBundle.php
+++ src/Pouce/UserBundle/PouceUserBundle.php
@@ -2,12 +2,21 @@
 
 namespace Pouce\UserBundle;
 
+use Pouce\UserBundle\DependencyInjection\Security\Factory\WsseFactory;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class PouceUserBundle extends Bundle
 {
-  public function getParent()
-  {
-    return 'FOSUserBundle';
-  }
+	public function getParent()
+	{
+		return 'FOSUserBundle';
+	}
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new WsseFactory());
+    }
 }
